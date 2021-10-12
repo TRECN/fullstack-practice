e551c056ed56c1b281b1ecdca9d457153753a690
TRECN <rishabh1103@outlook.com>
2021-10-12 17:33:25 +0530

    commit

--- test2/script.js
+++ test2/script.js
@@ -1,3 +1,20 @@
-const box=document.querySelector(".box");
-const customBtn=document.querySelector("#custom-btn");
-const img=document.querySelector(".image")
+const boxes = document.querySelector(".box");
+const defaultBtn = document.querySelector("#default-btn");
+const img = document.querySelector("img");
+function btnActive(){
+    defaultBtn.click();
+}
+defaultBtn.addEventListener("change",function(){
+    const file = this.file[0];
+    if(file){
+        const reader = new FileReader();
+        reader.onload = function(){
+            const result = reader.result;
+               img.src = result;
+               boxes.classList.add("active");
+        }
+        reader.readAsDataURL(file)
+    }
+    
+
+});
