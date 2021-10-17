5bbcf760d55bb390569e2565fb4c4ca4e880f964
TRECN <rishabh1103@outlook.com>
2021-10-17 23:37:10 +0530

    commit

--- js101/stringM.js
+++
@@ -1,9 +0,0 @@
-const name="rishabh raj tarun";
-console.log(name.search('h'))
-console.log(name.slice(4,10))
-console.log(name.slice(9))
-console.log(name.substr(10,6))
-console.log(name.toUpperCase())
-console.log(name.split(" "))
-console.log(name.length)
-console.log(name[10])
