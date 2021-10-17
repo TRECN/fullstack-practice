fa66f4c74afa19f88c08b3948d8ab97396246175
TRECN <rishabh1103@outlook.com>
2021-10-17 23:05:01 +0530

    commit

--- js101/condi.js
+++ js101/condi.js
@@ -1,8 +1,11 @@
-var a=10;
-var b=20;
-if(a==b){
-    console.log("rishabh")
-}else
-{
-    console.log("raj")
-}
+// var a=10;
+// var b=20;
+// if(a==b){
+//     console.log("rishabh")
+// }else
+// {
+//     console.log("raj")
+// }
+
+const a="RIshabh";
+if(a=="rishabh")
