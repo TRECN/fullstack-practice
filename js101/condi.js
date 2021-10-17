ade717b7eecd19abcf8371a620ac160060aaf563
TRECN <rishabh1103@outlook.com>
2021-10-17 23:10:06 +0530

    commit

---
+++ js101/condi.js
@@ -0,0 +1,27 @@
+// var a=10;
+// var b=20;
+// if(a==b){
+//     console.log("rishabh")
+// }else
+// {
+//     console.log("raj")
+// }
+
+// const a="RIshabh";
+// if(a=="rishabh"){
+//     console.log("rishabh");
+// }
+// else{
+//     console.log("raj");
+// }
+
+const age="18"
+if(age>=18){
+    console.log("allowed to vote");
+}
+if(age>=21){
+    console.log("can drink")
+}
+else{
+    console.log("not old enough to drink alcoholz")
+}
