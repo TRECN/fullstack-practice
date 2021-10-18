74d1b69dd63e1f789397dba2894f1612af18ba02
TRECN <rishabh1103@outlook.com>
2021-10-19 03:59:08 +0530

    commit

--- js101/arrIteration.js
+++ js101/arrIteration.js
@@ -1,9 +1,7 @@
 const arr=["one", "two", "three"]
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i])
-}
+
 //or
 
-arr.forEach(n=>{
-    console.log(arr[n])
-})
+arr.forEach(num=>{
+    console.log(num)
+});
