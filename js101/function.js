d3a4256842432f4098c3e1aa0f3ad3f0249cb6c0
TRECN <rishabh1103@outlook.com>
2021-10-19 21:17:05 +0530

    commit

--- js101/function.js
+++ js101/function.js
@@ -6,3 +6,13 @@ function addNumbers(num1,num2){
 const addT=addNumbers("46","67");
 console.log(addT)
 console.log(typeof(addT))
+
+//...rest
+
+function sum(...numbers){
+    let total=0
+    for(i in numbers){
+        total=total+numbers[i];
+    }
+    return total
+}
