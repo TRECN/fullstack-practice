1d0088108ca9004cc24fd503c9eeced48b5c6555
TRECN <rishabh1103@outlook.com>
2021-10-20 22:52:41 +0530

    commit

--- js201/scope.js
+++ js201/scope.js
@@ -2,7 +2,7 @@ const num=1;//can work outside
 if(num===1){
     //const greeting works only under this bracket
     const greeting ="hello";
-    //var is accessable outside this brackets
+    //var is accessible outside this brackets
     var greeting1="hi";
 }
 console.log(greeting1)
