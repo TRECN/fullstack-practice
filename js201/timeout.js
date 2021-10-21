7cb48f29ded6ad24f5331d22b9d4aef0e477a199
TRECN <rishabh1103@outlook.com>
2021-10-21 21:15:56 +0530

    commit

---
+++ js201/timeout.js
@@ -0,0 +1,18 @@
+// console.log("rishbah")
+// setTimeout(function(){
+//     console.log("rishabh")
+// },3000)
+
+let count =0;
+
+
+const myInterval=setInterval(function(){
+
+    console.log("Checking something...co")
+    count++;
+
+    if(count===3){
+        clearInterval(myInterval)
+        console.log("that was the last one")
+    }
+},2500)
