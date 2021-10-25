d2865349f69fd2c5b8e638b92e21f7fcdc48b77b
TRECN <rishabh1103@outlook.com>
2021-10-25 21:34:26 +0530

    commit

--- js201/destructuring.js
+++ js201/destructuring.js
@@ -6,11 +6,11 @@ console.log(months)
 
 const person3={
     "name":"rishabh",
-    "class":"10",
+    "classe":"10",
     "roll":"47"
 }
 //normal
-const name=person3['name']
+const namee=person3['name']
 const classes=person3['class']
 //destructuring
-const {name1,classe}=person3;
+const {name,classe}=person3;
