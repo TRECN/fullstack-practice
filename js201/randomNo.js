9e8175bf869e1dfc21b933facd70c37ba121a782
TRECN <rishabh1103@outlook.com>
2021-10-26 22:16:15 +0530

    commit

--- js201/randomNo.js
+++ js201/randomNo.js
@@ -1,6 +1,6 @@
 let number=prompt("guess a number")
 number=Number(number);
-let randomno=Math.floor(Math.random())
+let randomno=Math.floor(Math.random()*10)
 while(true){
     if(number===randomno){
         alert("you guessed a number")
