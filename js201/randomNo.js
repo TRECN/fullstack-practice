c04ad6485d642948e9c96cc26ae0a3d2e993b78d
TRECN <rishabh1103@outlook.com>
2021-10-27 23:11:05 +0530

    commit

--- js201/randomNo.js
+++
@@ -1,9 +0,0 @@
-const randomno=Math.floor(Math.random()*10);
-while(true){
-    let number=prompt("guess a number");
-    number=Number(number);
-    if(number===randomno){
-        alert("you guessed a number");
-        break;
-    }
-}
