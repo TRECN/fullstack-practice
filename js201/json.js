3ec099791e112aa2dcd51fab31738d7f75749042
TRECN <rishabh1103@outlook.com>
2021-10-28 21:56:23 +0530

    commit

--- js201/json.js
+++ js201/json.js
@@ -1,2 +1,17 @@
 //JSON JavaScript Object Notation
 //to share data
+const objects={
+    'name':'Rishabh',
+    'fav':['pizza','tacos','salmon'],
+    'age':'18',
+    'children':[
+        {
+            'name':'Rishabh',
+            'age':18,
+        },
+        {
+            'name':'raj',
+            'age':'18'
+        }
+    ]
+}
