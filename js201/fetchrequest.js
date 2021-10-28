dbef02a97285dbf08fa5a224b1dcce820501c7db
TRECN <rishabh1103@outlook.com>
2021-10-28 22:04:58 +0530

    commit

--- js201/fetchrequest.js
+++ js201/fetchrequest.js
@@ -1,3 +1,9 @@
 // fetch('www.website.com')
 // .then(...)
 // .then(...)
+
+fetch('https://swapi.dev/api/people/1/')
+    .then(response=>response.json())
+    .then(data=>{
+        console.log(data)
+    })
