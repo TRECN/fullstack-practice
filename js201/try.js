8c512f9b3f8e4e26325008df9ac40e0f2893ef19
TRECN <rishabh1103@outlook.com>
2021-10-28 21:19:12 +0530

    commit

--- js201/try.js
+++ js201/try.js
@@ -15,6 +15,15 @@ try{
 }catch(error){
     console.log("error is: ",error)
 }finally{
-    
+
 }
 console.log("show me!!")
+
+const num=10;
+try{
+    num+=10;
+}catch(error){
+    console.log("error is",error);
+}finally{
+    console.log(num)
+}
