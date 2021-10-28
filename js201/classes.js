3fd3a4f86dad562d854ebacbddbefc8a1496ecfe
TRECN <rishabh1103@outlook.com>
2021-10-28 21:48:18 +0530

    comit

--- js201/classes.js
+++ js201/classes.js
@@ -1,7 +1,13 @@
+
 class MyClassName{
+    
+    setName(name1){
+        this.name1=name1;
+    }
     speak(){
-        console.log("show")
+        console.log(`say my ${this.name1}`)
     }
 }
 const thing=new MyClassName();
+
 thing.speak()
