946708eb75b98456540ec838049bf8752b0708d3
TRECN <rishabh1103@outlook.com>
2021-10-29 21:04:37 +0530

    commit

--- js201/project/script.js
+++ js201/project/script.js
@@ -1,16 +1,13 @@
 let obj={}
-let num=Math.ceil(Math.random()*18);
+
 
 const names=document.getElementById('name')
 
+const random=()=>{
+    let num=Math.ceil(Math.random()*18);
 
-    fetch(`https://swapi.dev/api/people/${num}/`)
-        .then(response=>response.json())
-        .then(
-            dat=>{
-                obj=dat
-            }
-        )
+}
+    
 
 setTimeout(() => {
     console.log(obj['name'])
