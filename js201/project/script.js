4cfae124606976f4b6e30ba39d9f5316497ec498
TRECN <rishabh1103@outlook.com>
2021-10-29 20:44:00 +0530

    commit

--- js201/project/script.js
+++ js201/project/script.js
@@ -1,16 +1,13 @@
 let obj={}
 
-try{
     fetch('https://swapi.dev/api/people/1/')
         .then(response=>response.json())
-        .then(data=>{
-            console.log(typeof(data))
-            obj=data
-        })
-}
-catch(error){
-    console.log(error)
-}
-finally{
-    console.log(typeof(data))
-}
+        .then(
+            dat=>{
+                obj=dat
+            }
+        )
+
+setTimeout(() => {
+    console.log(obj)
+}, (5000));
