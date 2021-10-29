4f04305ba859b65806cec885b1cc27b5ff1f3daa
TRECN <rishabh1103@outlook.com>
2021-10-29 21:10:09 +0530

    commit

--- js201/project/script.js
+++ js201/project/script.js
@@ -2,9 +2,16 @@ let obj={}
 
 
 const names=document.getElementById('name')
-
+let num=Math.ceil(Math.random()*18);
+fetch(`https://swapi.dev/api/people/${num}/`)
+    .then(response=>response.json())
+    .then(
+        dat=>{
+            obj=dat
+        }
+    )
 const reset=()=>{
-    let num=Math.ceil(Math.random()*18);
+    num=Math.ceil(Math.random()*18);
     fetch(`https://swapi.dev/api/people/${num}/`)
         .then(response=>response.json())
         .then(
@@ -18,7 +25,14 @@ const reset=()=>{
             names.innerHTML=obj['name']
         
         }, (1000));
+        
 }
+setTimeout(() => {
+    console.log(obj['name'])
+    console.log(num)
+    names.innerHTML=obj['name']
+
+}, (1000));
     
 
 
