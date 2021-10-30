6b98c787570a548cc301d36142f5c113e7f88411
TRECN <rishabh1103@outlook.com>
2021-10-30 22:46:24 +0530

    commit

--- js201/project/script.js
+++ js201/project/script.js
@@ -28,28 +28,6 @@ const planet=()=>{
     )
 }
 
-const print=()=>{
-    setTimeout(() => {
-        console.log(num)
-        names.innerHTML=obj['name']
-        height.innerHTML=obj['height']
-        homeworld.innerHTML=world['name']
-    
-    }, (1000));
-}
-const reset=()=>{
-    names.innerHTML='xxxxx'
-        height.innerHTML='xxxxxx'
-        homeworld.innerHTML='xxxxxx'
-    if(num=='17'){
-        num++;
-    }
-    Fetch()
-    setTimeout(()=>{
-        planet() 
-        print()  
-    },1000)
-}
 
     
 
