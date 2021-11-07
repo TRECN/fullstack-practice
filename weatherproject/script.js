119fb4c4f733712da226abdfce2fe579fde16b21
TRECN <rishabh1103@outlook.com>
2021-11-07 23:16:43 +0530

    commit

--- weatherproject/script.js
+++ weatherproject/script.js
@@ -1,4 +1,18 @@
-fetch('api.openweathermap.org/data/2.5/weather?q=ranchi&appid=3bdb8ccccbc9a2bc4a2a07e4acdb6411')
+var headers = new Headers();
+headers.append("X-CSCAPI-KEY", "API_KEY");
+
+var requestOptions = {
+ method: 'GET',
+ headers: headers,
+ redirect: 'follow'
+};
+
+fetch("https://api.countrystatecity.in/v1/countries/IN/cities", requestOptions)
+.then(response => response.text())
+.then(result => console.log(result))
+.catch(error => console.log('error', error));
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3bdb8ccccbc9a2bc4a2a07e4acdb6411`)
     .then(response=>response.json())
     .then(data=>{
         console.log(data)
