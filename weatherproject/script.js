e117ae738bf9731ee164fe146e4d54affbd9298a
TRECN <rishabh1103@outlook.com>
2021-11-07 23:22:58 +0530

    commit

--- weatherproject/script.js
+++ weatherproject/script.js
@@ -1,16 +1,16 @@
-var headers = new Headers();
-headers.append("X-CSCAPI-KEY", "API_KEY");
+// var headers = new Headers();
+// headers.append("X-CSCAPI-KEY", "API_KEY");
 
-var requestOptions = {
- method: 'GET',
- headers: headers,
- redirect: 'follow'
-};
+// var requestOptions = {
+//  method: 'GET',
+//  headers: headers,
+//  redirect: 'follow'
+// };
 
-fetch("https://api.countrystatecity.in/v1/countries/IN/cities", requestOptions)
-.then(response => response.text())
-.then(result => console.log(result))
-.catch(error => console.log('error', error));
+// fetch("https://api.countrystatecity.in/v1/countries/IN/cities", requestOptions)
+// .then(response => response.text())
+// .then(result => console.log(result))
+// .catch(error => console.log('error', error));
 
 // fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3bdb8ccccbc9a2bc4a2a07e4acdb6411`)
 //     .then(response=>response.json())
