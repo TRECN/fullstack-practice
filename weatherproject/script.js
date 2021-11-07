915ce3718da592b20cd2d6c68be26affe560c682
TRECN <rishabh1103@outlook.com>
2021-11-07 23:17:05 +0530

    commit

--- weatherproject/script.js
+++ weatherproject/script.js
@@ -12,8 +12,8 @@ fetch("https://api.countrystatecity.in/v1/countries/IN/cities", requestOptions)
 .then(result => console.log(result))
 .catch(error => console.log('error', error));
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3bdb8ccccbc9a2bc4a2a07e4acdb6411`)
-    .then(response=>response.json())
-    .then(data=>{
-        console.log(data)
-    })
+// fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3bdb8ccccbc9a2bc4a2a07e4acdb6411`)
+//     .then(response=>response.json())
+//     .then(data=>{
+//         console.log(data)
+//     })
